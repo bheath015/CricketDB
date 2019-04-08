73c55d0e803f74127186da8837f337e12d15b4ce
bheath015 <31784768+bheath015@users.noreply.github.com>
2019-04-08 18:25:49 -0400

    adding webapp template

---
+++ webapp/Code/Analyzer.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+"""
+The CryptoGuru Investment Analyzer
+"""
+
+from Input import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+from io import BytesIO
+import base64
+
+class Crypto_Currency_Analyzer:
+
+  def __init__(self):
+    self.features = ["OPEN", "HIGH", "LOW", "CLOSE", "VOL"]
+    self.available_currencies = [c[:-4] for c in os.listdir('Data')]
+
+  def data_frame(self, feature="OPEN", currencies=None, start_date=None, end_date=None):
+    """
+    Returns and sets a dataframe for the selected feature and currencies.
+      Features:
+        - OPEN (default)
+        - HIGH
+        - LOW
+        - CLOSE
+        - VOL
+      Shows data for previous year as default.
+    """
+    # Desired dates; if none listed, past year as default
+    if not start_date:
+      start_date = date(1)
+    if not end_date:
+      end_date = date()
+
+    # Check all inputs allowable
+    allowable_inputs(self, feature, currencies, start_date, end_date)
+
+    # Construct DataFrame
+    dates = pd.date_range(start_date, end_date)
+    df = pd.DataFrame(index = dates)
+
+    # Join columns using 'DATE' as index
+    # Rename feature to currency name
+    for c in currencies:
+      df = df.join(pd.read_csv("Data/"+c+".txt", index_col="DATE",
+                               parse_dates=True, usecols=["DATE", feature],
+                               na_values=["nan"]))
+      df = df.rename(columns={feature: c})
+    df = self.fill_incomplete(df)
+    self.df = df.dropna()
+    return df.dropna()
+
+  def fill_incomplete(self, df):
+    """
+    Fills empty values in Dataframe df with 'NaN'
+    """
+    df.fillna(method="ffill", inplace=True)
+    df.fillna(method="bfill", inplace=True)
+    return df
+
+  def normalize(self):
+    """
+    Normalize stock prices using first row of dataframe.
+    """
+    self.df = self.df / self.df.ix[0,:]
+    return self.df / self.df.ix[0,:]
+
+  def plot_rolling_mean(self, currency=None, window=20, bollinger_bands=True):
+    """
+    Returns file data of a plot of the rolling mean of the selected currency
+    Uses default window size of 20 for calculating mean
+    Includes bollinger bands (showing standard deviation of average) by default
+      Deactivate bands with bollinger_bands = False
+    """
+    if not currency:
+      print("[-] Select currency to plot")
+      return
+    if not currency in self.df:
+      print("[-] Currency %s not in data frame." % currency)
+      return
+    rm = pd.DataFrame(pd.rolling_mean(self.df[currency], window=window))
+
+    if bollinger_bands:
+      std = pd.DataFrame(pd.rolling_std(self.df[currency], window=window))
+      upper_band = pd.DataFrame(rm + std * 2)
+      lower_band = pd.DataFrame(rm - std * 2)
+
+      rm = rm.rename(columns={currency: 'Rolling mean'})
+      upper_band = upper_band.rename(columns={currency: 'Upper band'})
+      lower_band = lower_band.rename(columns={currency: 'Lower band'})
+      rm = rm.join(upper_band).join(lower_band)
+
+    title = currency + " rolling mean"
+    return self.plot_data_frame(df=rm, title=title)
+
+
+  def plot_data_frame(self, df="NONE", title="Cryptocurrency values", show=False):
+    """
+    Generates plot of the given data frame or current df attribute of self.
+      - [show] Does not show plot by default. In order to show, show=True
+    """
+    if type(df)==str:
+      df = self.df
+    p = df.plot(title=title, fontsize=12)
+    p.set_xlabel("Date")
+    p.set_ylabel("Value")
+    p.legend(loc='upper left')
+    if show:
+      plt.show()
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    return figdata_png
+
+
+if __name__=='__main__':
+  cca = Crypto_Currency_Analyzer()
+  cca.data_frame(currencies=["Bitcoin"])
+  cca.plot_data_frame(show=True)
---
+++ webapp/routes.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, request
+
+from Code.Analyzer import *
+from Twitter.twitter_searcher import *
+from os import listdir
+import datetime
+
+app = Flask(__name__)
+
+#  The main view of CryptoGuru Invemestment Analyzer
+@app.route('/', methods = ['GET', 'POST'])
+def landing():
+    ts = Twitter_Searcher()
+    tweet_id, twitter_user = ts.get_tweet()
+    href_output = "https://twitter.com/" + str(twitter_user) + "/status/" + str(tweet_id);
+    return render_template('landing.html',
+      href_hail_mary = href_output, 
+      id_output = tweet_id,
+      handle_output = twitter_user)
+
+# The main view of CryptoGuru Invemestment Analyzer
+@app.route('/investment', methods = ['GET', 'POST'])
+def investment():
+  cca = Crypto_Currency_Analyzer()
+
+  ###############################
+  # Get start/end dates
+  #  - default, previous 3 months
+  ###############################
+  start_date = request.form.get('startDate')
+  end_date = request.form.get('endDate')
+  if not start_date:
+    date = datetime.datetime.now()
+    end_date = '%04d-%02d-%02d' % (date.year, date.month, date.day)
+    start_date = '%04d-%02d-%02d' % (date.year, date.month - 3, date.day)
+
+  ###############################
+  # Get currencies
+  ###############################
+  select = request.form.getlist('currency')
+  if not select:
+    select = ['Bitcoin']
+  select = [str(s) for s in select]
+
+  ###############################
+  # Get feature
+  ###############################
+  feature = request.form.get('features')
+  if not feature:
+    feature = "OPEN"
+  feature = str(feature)
+
+  ###############################
+  # Normalize checkbox
+  ###############################
+  norm = request.form.get('normalize')
+
+  ###############################
+  # Rolling mean checkbox/slider
+  ###############################
+  rolling_mean = request.form.get('rollingMean')
+  rm_window = request.form.get('rm_window')
+  if not rm_window:
+    rm_window = 20
+  else:
+    rm_window = int(rm_window)
+
+  ###############################
+  # Generate graph results
+  # Default graph:
+  # - Bitcoin
+  # - Open
+  # - Previous 3 months
+  ###############################
+  cca.data_frame(currencies=select, feature=feature, start_date=start_date)
+  if norm:
+    cca.normalize()
+  if rolling_mean:
+    result = cca.plot_rolling_mean(currency=select[0], window=rm_window)
+  else:
+    result = cca.plot_data_frame()
+ 
+  return render_template('investment.html',
+			result=result, 
+			currencies=cca.available_currencies, 
+			feature=feature,
+			start_date=start_date, 
+			end_date=end_date, 
+			select=select, 
+			norm=norm, 
+			rolling_mean=rolling_mean, 
+			rm_window=rm_window)
+
+if __name__=='__main__':
+  app.run(debug=True)
